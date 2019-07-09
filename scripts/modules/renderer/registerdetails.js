b6ffa20ce652a63b82be9c0c6d4a2e6eb4c8a6e3
Victor Davidsson <kontakt@victordavidsson.se>
2019-07-09 15:36:39 +0200

    Removed unused reject from Promises
    These Promises should always resolve, if an error occurs it will notify in console instead and keep on loading.

--- scripts/modules/renderer/registerdetails.js
+++ scripts/modules/renderer/registerdetails.js
@@ -2,7 +2,7 @@ let fs = require("fs");
 let readPath = global.__basedir + "/scripts/modules/details/";
 
 function handler(loaded_details) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
     fs.readdir(readPath, function(err, files) {
       for (let i = 0; i < files.length; i++) {
         require(readPath + files[i])(loaded_details);
--- scripts/modules/renderer/registerplugins.js
+++ scripts/modules/renderer/registerplugins.js
@@ -2,7 +2,7 @@ let fs = require("fs");
 let readPath = global.__basedir + "/scripts/modules/plugins/";
 
 function handler(loaded_plugins) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
     fs.readdir(readPath, function(err, files) {
       for (let i = 0; i < files.length; i++) {
         require(readPath + files[i])(loaded_plugins);

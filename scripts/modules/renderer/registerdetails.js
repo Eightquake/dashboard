01e1ed76aeba9726cb91666ae9576d4cfc35f301
Victor Davidsson <kontakt@victordavidsson.se>
2019-07-16 15:58:43 +0200

    Made it so that a detail can be either a .js-module or a .json-file
    If a detail does not need a function to be run it can just be a .json file, but if it does it can still be a .js file and be run as a function. It won't be run more than once though.

--- scripts/modules/renderer/registerdetails.js
+++ scripts/modules/renderer/registerdetails.js
@@ -1,12 +1,21 @@
 let fs = require("fs");
 let readPath = global.__basedir + "/scripts/modules/details/";
 
+let jsonRegex = /\.json$/gi;
+let jsRegex = /\.js$/gi;
+
 function handler(loaded_details) {
   return new Promise(function(resolve) {
     fs.readdir(readPath, function(err, files) {
       for (let i = 0; i < files.length; i++) {
-        require(readPath + files[i])(loaded_details);
-        loaded_details.set(files[i], require(readPath + [files[i]])());
+        if(jsonRegex.test(files[i])) {
+          /* The file is a JSON-file, as it ends with .json or .JSON. As a JSON-file we can just import it and there's no need to call it */
+          loaded_details.set(files[i], require(readPath + [files[i]]));
+        }
+        else if(jsRegex.test(files[i])) {
+          /* The file is a JavaScript-file, as it ends with .js or .JS. We should call the function it hopefully exports */
+          loaded_details.set(files[i], require(readPath + [files[i]])());
+        }
       }
       resolve();
     });

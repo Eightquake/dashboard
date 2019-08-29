ffd548611858095461eb850cf27db23e8d3e6726
Victor Davidsson <kontakt@victordavidsson.se>
2019-08-29 15:00:48 +0200

    Updated webpack config
    Target electron-renderer was giving me trouble as I don't have node integration.

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,5 +1,14 @@
+const path = require("path");
+
 // define child rescript
 module.exports = config => {
-    config.target = 'electron-renderer';
-    return config;
-  }
+  //config.target = "electron-renderer";
+  config.resolve = {
+    alias: {
+      "@project-app": path.resolve(__dirname),
+      "@project-src": path.resolve(__dirname, "/src")
+    }
+  };
+
+  return config;
+};

66a3aa10006fde01271faf6ac1f00596e1aa7ec0
Victor Davidsson <kontakt@victordavidsson.se>
2019-08-29 15:10:10 +0200

    Update index.js

--- src/js/renderer/index.js
+++ src/js/renderer/index.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-undef, no-unused-vars, import/first */
+/**
+ * The renderer process. It registers some global variables and on document load it starts the chain of functions that is needed to make the app work.
+ * @category Renderer
+ * @module renderer
+ * @author Victor Davidsson
+ *
+ */
+
+/**
+ * Error handler script, that any code can emit an event on. When an event error, warn, or info is emitted a popup will be shown to the user.
+ * All scripts should use this to communicate with the user, instead of using the § or similar.
+ * @see {@link module:problemHandler} for more information.
+ * @category Renderer
+ * @global
+ */
+import problemHandler from "./problemHandler.js";
+global.problem = problemHandler;
+
+import fillGrid from "./fillGrid.js";
+import initGrid from "./initGrid.js";
+
 let registeredDetails,
   registeredPlugins,
   loadedDetails = new Map(),
